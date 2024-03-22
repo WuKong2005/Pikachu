4662686a8e4e7cad7613cade8a0af8d3d606f2f1
TCMeen01 <tcminh23@clc.fitus.edu.vn>
2024-03-22 21:28:11 +0700

    update Visual.cpp and .h

--- Visual.h
+++ Visual.h
@@ -10,12 +10,15 @@ enum COLOR {
   YELLOW,
   BLUE
 };
+const string TEXT_BLACK = "\x1B[0m";
 
 // FRONTEND 
 string GAME_LOGO[7];
 const string MAIN_MENU;
-const string INSIDE_PLAY_GAME;
-const string INSIDE_NEW_GAME;
+const string PLAY_GAME;
+const string GAME_MODE;
+const string LEADERBOARD;
+const string HELP;
 
 // hide the Cursor in Console
 void hideCursor();
@@ -23,4 +26,8 @@ void hideCursor();
 struct visual 
 {
   void printLogo();
+  void printPlayGame();
+  void printGameMode();
+  void printLeaderBoard();
+  void printHelp();
 };
