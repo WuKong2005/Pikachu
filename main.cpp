8616040f6a131ba7f341b2ac3f612587be6a2a4b
TCMeen01 <tcminh23@clc.fitus.edu.vn>
2024-04-02 18:11:09 +0700

    update

--- main.cpp
+++ main.cpp
@@ -4,30 +4,7 @@
 
 int main() {
     srand(time(NULL));
-    // SetConsoleScreenBufferSize(console, )
-    // initializeProgram();
-    SetConsoleOutputCP(65001);
     printLogo();
     MAIN_MENU_CONTROL();
-    // playSound(MENU);
-
-    printUsername();
-    system("pause");
-    
-
-
-    // SetConsoleOutputCP(437);
-    // game g(HARD);
-    // system("pause");
-    // Sleep(1000);
-    // g.startGame();
-    SetConsoleOutputCP(437);
-    game g(HARD);
-    // game g;
-    // cerr << g.verifySaveFile() << '\n';
-    // g.loadGame();
-    system("pause");
-    Sleep(1000);
-    g.startGame();
     return 0;
 }
